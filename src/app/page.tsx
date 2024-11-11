af542c266caaff4c02f9285b367f9fa28c19f92c
Diego Mirhan <mirhan.diego@gmail.com>
2024-11-11 10:35:36 -0300

    portfolio begin

---
+++ src/app/page.tsx
@@ -0,0 +1,51 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import Link from "next/link";
+import { Button, ButtonGroup } from "@nextui-org/button";
+export default function Home() {
+  return (
+    <>
+      <header>
+        <Navbar>
+          <NavbarBrand>
+            <p className="font-bold text-inherit">~/</p>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Features
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href="#" aria-current="page">
+                Customers
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Integrations
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarContent justify="end">
+            <NavbarItem className="hidden lg:flex">
+              <Link href="#">Login</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button as={Link} color="primary" href="#" variant="flat">
+                Sign Up
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+      </header>
+    </>
+  );
+}
