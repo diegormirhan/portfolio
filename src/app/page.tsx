9971e98b12f0a0edead8883140483d338381c17f
diegormirhan <mirhan.diego@gmail.com>
2025-03-14 18:00:55 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
     </div>
   );
 }
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-    extend: {
-        colors: {
-            'main-bg-color': '#F1F2FA',
-        }
-    }
-}
